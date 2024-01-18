024c89c38e485f97539bea37df2df05c0cd55900
TrinhDanh <trinhdanh04@gmail.com>
2024-01-18 10:24:40 +0700

    test 26

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
+const PLATFORM = {
+    MACOS: "MacOS",
+    IOS: "iOS",
+    ANDROID: "Android",
+    WINDOWS: "Windows",
+    LINUX: "Linux",
+}
+
+// ==== Get referral code from url parameter
 const searchParams = new URLSearchParams(window.location.search);
 let referral = '';
 
@@ -10,29 +19,30 @@ if (searchParams.has('referral')) {
     redirectLink.setAttribute("href", "twss-customer://twssolutions.us?referral=" + referral);
 }
 
-// auto redirect
+// ==== Auto redirect go to app (Sign up page) or download link
 document.addEventListener("DOMContentLoaded", function () {
-    var linkElement = document.getElementById("yourLinkId");
-    if (linkElement) {
-        linkElement.click();
-    }
+    onRedirect();
 });
 
-function onCopyReferralCode() {
-    navigator.clipboard.writeText(referral);
-}
-
-function redirect() {
-    myURL();
-    var result = document.getElementById("result");
-    result.innerHTML = "<b> The page will redirect after delay of 5 seconds";
-}
-
-function myURL() {
-    var linkElement = document.getElementById("yourLinkId");
-    linkElement.click();
-
-
+function onRedirect() {
+    try {
+        var linkElement = document.getElementById("yourLinkId");
+        if (linkElement) {
+            linkElement.click();
+        }
+    } catch (error) {
+        let platform = onGetOS();
+        switch (platform) {
+            case PLATFORM.ANDROID:
+                window.location = 'https://twss-ohio-public-storage.s3.us-east-2.amazonaws.com/mobile-app/TWSSolutions_Customer.apk'; // android download link
+                break;
+            case PLATFORM.IOS:
+                window.location = 'itms-beta://beta.itunes.apple.com/v1/app/6466731245'; // ios download link
+                break;
+            default:
+                break;
+        }
+    }
     // window.location = 'twss-customer://twssolutions.us';
     // // window.open("twss-customer://twssolutions.us", "_blank");
     // const url =
@@ -40,12 +50,33 @@ function myURL() {
     // window.open(url, "_blank");
 }
 
-function redirect2() {
-    setTimeout(myURL2, 5000);
-    var result = document.getElementById("result2");
-    result.innerHTML = "<b> The page will redirect after delay of 5 seconds";
+// ==== User can copy referral code if redirect link not work
+function onCopyReferralCode() {
+    navigator.clipboard.writeText(referral);
 }
 
-function myURL2() {
-    window.location.href = 'https://google.com';
+// ==== Get current platform
+function onGetOS() {
+    // console.log("user agent data: ", window?.navigator?.userAgentData.mobile)
+
+    const userAgent = window.navigator.userAgent,
+        platform = window.navigator?.userAgentData?.platform || window.navigator.platform,
+        macosPlatforms = ['macOS', 'Macintosh', 'MacIntel', 'MacPPC', 'Mac68K'],
+        windowsPlatforms = ['Win32', 'Win64', 'Windows', 'WinCE'],
+        iosPlatforms = ['iPhone', 'iPad', 'iPod'];
+    let os = null;
+
+    if (macosPlatforms.indexOf(platform) !== -1) {
+        os = PLATFORM.MACOS;
+    } else if (iosPlatforms.indexOf(platform) !== -1) {
+        os = PLATFORM.IOS;
+    } else if (windowsPlatforms.indexOf(platform) !== -1) {
+        os = PLATFORM.WINDOWS;
+    } else if (/Android/.test(userAgent)) {
+        os = PLATFORM.ANDROID;
+    } else if (/Linux/.test(platform)) {
+        os = PLATFORM.LINUX;
+    }
+
+    return os;
 }
